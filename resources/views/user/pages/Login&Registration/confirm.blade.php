5ec52baaa4d9e51ef68db3ac597725d24998017d
Dmytro Kardava <dima.kardava@gmail.com>
2021-01-04 15:40:05 +0100

    change confirm email view

--- resources/views/user/pages/Login&Registration/confirm.blade.php
+++ resources/views/user/pages/Login&Registration/confirm.blade.php
@@ -1,5 +1,10 @@
 @extends('user.layouts.auth_user')
 
 @section('content')
-    <h3>Вам отправлено сообщение c подтверждением на емейл.</h3>
+    <h3>Вам необходимо подтвердить ваш e-mail. Для этого перейдите по ссылке в письме.</h3>
+
+    <form method="POST" action="#">
+        @csrf
+        <button type="submit" class="btn btn-primary">Отправить заново</button>
+    </form>
 @endsection
