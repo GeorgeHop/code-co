4866e4dc3f493bba6db2f86598e6fd297d2da2f2
GeorgeHope <georgehopekek@gmail.com>
2021-01-25 23:26:30 +0100

    Added user panel nav

--- resources/views/user/pages/UserPanel/user_main.blade.php
+++ resources/views/user/pages/UserPanel/user_main.blade.php
@@ -4,11 +4,27 @@
     <section id="pricing" class="custom-section" data-stellar-background-ratio="0.5">
         <div class="container">
             <div class="row">
+                <div class="col-md-2">
+
+                </div>
+                <div class="col-md-8">
+                    <div class="user-panel-nav">
+                        <a class="user-panel-nav-link">Список курсов</a>
+                        <a class="user-panel-nav-link">Корзина</a>
+                        <a class="user-panel-nav-link">Профиль</a>
+                    </div>
+                </div>
+                <div class="col-md-2">
+
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-md-12 col-sm-12">
                     <div class="section-title">
                         <h1>Список ваших курсов</h1>
                     </div>
                 </div>
+            </div>
                 @foreach($courses as $course)
                     @if($course)
                         <div class="col-md-4 col-sm-6">
@@ -35,6 +51,5 @@
                     @endif
                 @endforeach
             </div>
-        </div>
     </section>
 @endsection
